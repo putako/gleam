0915521442a98b4998b76061562107bc50ca36fb
Jared Harless <Jared.e.harless@gmail.com>
2016-10-17 21:34:36 -0700

    what is happening here

--- db/migrate/20161018024005_create_charactersheets.rb
+++ db/migrate/20161018024005_create_charactersheets.rb
@@ -3,18 +3,18 @@ class CreateCharactersheets < ActiveRecord::Migration[5.0]
     create_table :charactersheets do |t|
       t.string :player_name,    null: false
       t.string :character_name, null: false
-      t.integer :ability_str,
-      t.integer :ability_dex,
-      t.integer :ability_con,
-      t.integer :ability_int,
-      t.integer :ability_wis,
-      t.integer :ability_cha,   # these are not known at creation so can be null .... right? FUUUCK
-      t.integer :bonus_str,
-      t.integer :bonus_dex,
-      t.integer :bonus_con,
-      t.integer :bonus_int,
-      t.integer :bonus_wis,
-      t.integer :bonus_cha,
+      t.integer :ability_str
+      t.integer :ability_dex
+      t.integer :ability_con
+      t.integer :ability_int
+      t.integer :ability_wis
+      t.integer :ability_cha   # these are not known at creation so can be null .... right? FUUUCK
+      t.integer :bonus_str
+      t.integer :bonus_dex
+      t.integer :bonus_con
+      t.integer :bonus_int
+      t.integer :bonus_wis
+      t.integer :bonus_cha
 
       t.timestamps              null: false
     end
