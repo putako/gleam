3a12f630a91c9123bf30478ea81c4bdd3ce2ef31
Jared Harless <Jared.e.harless@gmail.com>
2016-10-17 22:05:28 -0700

    made it do stuff

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
 	def index
-
+		@charactersheets = Charactersheet.all.limit(10)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dashboard#index'
+  resources :dashboard, only: [ :index ]
 end
